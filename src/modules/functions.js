fd3024cdc62f32c0136623d422b1566ccd7f71f0
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-05-10 22:40:45 -0300

    Comment module.exports

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -4,7 +4,6 @@ import refreshImgSrc from '../refresh.png';
 import trashImgSrc from '../trash.png';
 import { addCheckboxListenerOnLoad } from './check_box.js';
 
-const { test } = require('node:test');
 export const createFrame = () => {
   const h1Container = document.querySelector('.h1-container');
   const refreshImg = document.createElement('img');
@@ -179,6 +178,7 @@ export const createEmptyUl = () => {
   container.appendChild(ul);
 };
 
-module.exports = {
+/* module.exports = {
   addTask, editTask, updateLocalStorage, addLocalStorage,
 };
+ */
